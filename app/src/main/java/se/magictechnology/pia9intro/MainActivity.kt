eeba8abfa9e44cb67e424fb67b1d96ca48a7e6a8
Bill Mårtensson <bill@magictechnology.se>
2021-01-08 13:37:20 +0100

    Layout

--- app/src/main/java/se/magictechnology/pia9intro/MainActivity.kt
+++ app/src/main/java/se/magictechnology/pia9intro/MainActivity.kt
@@ -2,16 +2,37 @@ package se.magictechnology.pia9intro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val toptext = findViewById<TextView>(R.id.theTopText)
+        val topbutton = findViewById<Button>(R.id.theTopButton)
 
 
 
+
+
+        toptext.text = "Hejsan hej!!!"
+
+        topbutton.setOnClickListener {
+            toptext.text = "BANAN!!!"
+        }
 
 
     }
+
+
+
+
+
+
 }
